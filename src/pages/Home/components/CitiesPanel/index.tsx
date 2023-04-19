59297b0b738b08f67917f5308d6d209a5296ce55
Loan Matteus <loan.matteusz@gmail.com>
2023-04-19 14:39:47 -0300

    fix: adjust emptydata feedback text

--- src/pages/Home/components/CitiesPanel/index.tsx
+++ src/pages/Home/components/CitiesPanel/index.tsx
@@ -4,14 +4,14 @@ import { CitiesPanelContent, EmptyData } from "./styles";
 import { CitiesPanelProps, FormatedDataProps } from "./types";
 
 export const CitiesPanel = ({ data }: CitiesPanelProps) => {
-    const formatedData = prepareDataByLetter(data) as FormatedDataProps[];
+    const formatedData = prepareDataByLetter(data) as FormatedDataProps[] || [];
     return (
         <CitiesPanelContent>
             {
                 formatedData?.map(data => <CollapseList key={data.letter} letter={data.letter} cities={data.cities} />)
             }
             {
-                formatedData?.length === 0 && <EmptyData>Não há nenhuma cidade que satisfaça a busca</EmptyData>
+                formatedData?.length === 0 && <EmptyData>Nenhuma cidade encontrada</EmptyData>
             }
         </CitiesPanelContent>
     );
